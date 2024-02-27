b0f4d76f023e9993cd62cad3ccc6ffef2ef6be20
Fraser Sequeira <leo27fraser@gmail.com>
2024-02-27 16:30:46 +0530

    Update WebappApplication.java

--- src/main/java/com/fraser/WebappApplication.java
+++ src/main/java/com/fraser/WebappApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ComponentScan("com.*")
 @EnableAutoConfiguration
+@EnableSwagger2
 public class WebappApplication {
 
 	public static void main(String[] args) {
