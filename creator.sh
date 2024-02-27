4435f4bd50fb0768c02961ec147647ad1f403846
Fraser Sequeira <leo27fraser@gmail.com>
2024-02-27 19:17:38 +0530

    modified

--- creator.sh
+++ creator.sh
@@ -4,7 +4,8 @@ Red='\033[0;31m'
 NC='\033[0m'
 
 sudo su -s "$0"
-
+printf "$Green Remove dangling images $NC"
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 printf "$Green Enter a username for kafka-manager $NC"
 read username
 
--- src/main/java/com/fraser/camel/KafkaConsumer.java
+++ src/main/java/com/fraser/camel/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.fraser.camel;
 
+import java.util.Map;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,21 +38,21 @@ public class KafkaConsumer extends RouteBuilder{
         //         System.out.println(exch1.getIn().getBody());
         //     }           
         // })
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
         //.log("Consumer is running");
 
         from(kafkaProps.getAnalytics())
         .routeId("analytics-intake")
         .autoStartup(false)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
 
 
         from(kafkaProps.getS3dump())
         .routeId("s3-dump-intake")
         .autoStartup(false)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
  
         
@@ -58,54 +60,54 @@ public class KafkaConsumer extends RouteBuilder{
         .routeId("send-to-email-sms-whatsapp-topic-1")
         .autoStartup(true)
         .multicast().parallelProcessing()
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .to(kafkaProps.getEmail1(), kafkaProps.getSms1(), kafkaProps.getWhatsapp1());
 
         from(kafkaProps.getSubscription())
         .routeId("send-to-email-sms-whatsapp-topic-2")
         .autoStartup(false).multicast().parallelProcessing()
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .to(kafkaProps.getEmail2(), kafkaProps.getSms2(), kafkaProps.getWhatsapp2());
 
         from(kafkaProps.getEmail1())
         .routeId("fetch-from-emails-topic-1")
         .autoStartup(true)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
 
         from(kafkaProps.getEmail2())
         .routeId("fetch-from-emails-topic-2")
         .autoStartup(false)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
 
         from(kafkaProps.getSms1())
         .routeId("fetch-from-sms-topic-1")
         .autoStartup(true)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
 
         from(kafkaProps.getSms2())
         .routeId("fetch-from-sms-topic-2")
         .autoStartup(false)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
 
         from(kafkaProps.getWhatsapp1())
         .routeId("fetch-from-whatsapp-topic-1")
         .autoStartup(true)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
 
         from(kafkaProps.getWhatsapp2())
         .routeId("fetch-from-whatsapp-topic-2")
         .autoStartup(false)
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .bean(OrderProcessor.class, "processor");
 
         from(kafkaProps.getNewarrivals()).routeId("send-new-arrivals-details")
         .autoStartup(true).multicast().parallelProcessing()
-        .unmarshal().json(JsonLibrary.Jackson)
+        .unmarshal().json(JsonLibrary.Jackson).convertBodyTo(String.class)
         .to(kafkaProps.getEmail1(),kafkaProps.getSms1(), kafkaProps.getWhatsapp1());
     }
     
